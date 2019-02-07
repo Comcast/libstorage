1486cce66cc72c0da6ca2b8d45b0aa26444f19f5
Chris Holcombe <xfactor973@gmail.com>
2019-02-07 15:52:29 -0800

    Add license header

--- src/isilon.rs
+++ src/isilon.rs
@@ -4,6 +4,24 @@
 //! Note: Isilon has a unique api that is self describing.  This could allow someone
 //! in the future to write a parser for the api to generate a complete library binding.
 
+/**
+* Copyright 2019 Comcast Cable Communications Management, LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
+
 use std::rc::Rc;
 
 use futures::Future;
