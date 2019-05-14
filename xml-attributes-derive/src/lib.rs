0c456958fa8eed797053460e452beccd8ca0035c
Sravanthi D <sravanthi_dandamudi@comcast.com>
2019-05-14 09:23:42 -0500

    vnx cifs server queries

--- xml-attributes-derive/src/lib.rs
+++ xml-attributes-derive/src/lib.rs
@@ -1,3 +1,4 @@
+extern crate proc_macro;
 /**
 * Copyright 2019 Comcast Cable Communications Management, LLC
 *
@@ -15,16 +16,14 @@
 *
 * SPDX-License-Identifier: Apache-2.0
 */
-
 extern crate proc_macro2;
-extern crate proc_macro;
 #[macro_use]
 extern crate quote;
 extern crate syn;
 
-use quote::ToTokens;
 use proc_macro::TokenStream;
 use proc_macro2::{Literal, Spacing, Span, TokenNode, TokenTree};
+use quote::ToTokens;
 use syn::{Data, Ident, Type};
 
 #[proc_macro_derive(FromXmlAttributes)]
@@ -50,7 +49,7 @@ fn impl_xml(ast: &syn::DeriveInput) -> quote::Tokens {
 
 fn union_fields(_name: &syn::Ident, _data: &syn::DataUnion) -> quote::Tokens {
     let mut result = Vec::new();
-    result.push(quote!{
+    result.push(quote! {
         panic!("not implemented");
     });
     quote! {
@@ -60,7 +59,7 @@ fn union_fields(_name: &syn::Ident, _data: &syn::DataUnion) -> quote::Tokens {
 
 fn enum_fields(_name: &syn::Ident, _variants: &syn::DataEnum) -> quote::Tokens {
     let mut result = Vec::new();
-    result.push(quote!{
+    result.push(quote! {
         panic!("not implemented");
     });
     quote! {
@@ -73,31 +72,38 @@ fn impl_struct_xml_fields(name: &syn::Ident, fields: &syn::Fields) -> quote::Tok
     for field in fields.iter() {
         let ident = &field.ident;
         let ident_type = match field.clone().ty {
-            Type::Path(p) => if let Some(i) = p.path.segments.clone().into_iter().next() {
-                Some(i.ident)
-            } else {
-                None
-            },
+            Type::Path(p) => {
+                if let Some(i) = p.path.segments.clone().into_iter().next() {
+                    Some(i.ident)
+                } else {
+                    None
+                }
+            }
             _ => None,
         };
         let u_64 = Ident::new("u64", Span::def_site());
         let f_64 = Ident::new("f64", Span::def_site());
         let string = Ident::new("String", Span::def_site());
+        let boolean = Ident::new("bool", Span::def_site());
 
         // Setup the fields
         match ident_type {
             Some(i_type) => {
                 if i_type == u_64 {
-                    result.push(quote!{
+                    result.push(quote! {
                         let mut #ident = 0;
                     });
                 } else if i_type == f_64 {
-                    result.push(quote!{
+                    result.push(quote! {
                         let mut #ident = 0.0;
                     });
                 } else if i_type == string {
-                    result.push(quote!{
+                    result.push(quote! {
                         let mut #ident = String::new();
+                    });
+                } else if i_type == boolean {
+                    result.push(quote! {
+                        let mut #ident = false;
                     });
                 } else {
                     // Uncomment me to debug why some fields may be missing
@@ -112,7 +118,7 @@ fn impl_struct_xml_fields(name: &syn::Ident, fields: &syn::Fields) -> quote::Tok
     }
 
     // Match the fields
-    result.push(quote!{
+    result.push(quote! {
         for a in attrs
     });
 
@@ -120,10 +126,11 @@ fn impl_struct_xml_fields(name: &syn::Ident, fields: &syn::Fields) -> quote::Tok
         TokenTree {
             span: Span::def_site(),
             kind: TokenNode::Op('{', Spacing::Joint),
-        }.into_tokens(),
+        }
+        .into_tokens(),
     );
 
-    result.push(quote!{
+    result.push(quote! {
         let item = a?;
         let val = String::from_utf8_lossy(&item.value);
         match item.key
@@ -133,22 +140,27 @@ fn impl_struct_xml_fields(name: &syn::Ident, fields: &syn::Fields) -> quote::Tok
         TokenTree {
             span: Span::def_site(),
             kind: TokenNode::Op('{', Spacing::Joint),
-        }.into_tokens(),
+        }
+        .into_tokens(),
     );
 
     for field in fields.iter() {
         let ident = &field.ident;
         let ident_type = match field.clone().ty {
-            Type::Path(p) => if let Some(i) = p.path.segments.clone().into_iter().next() {
-                Some(i.ident)
-            } else {
-                None
-            },
+            Type::Path(p) => {
+                if let Some(i) = p.path.segments.clone().into_iter().next() {
+                    Some(i.ident)
+                } else {
+                    None
+                }
+            }
             _ => None,
         };
         let u_64 = Ident::new("u64", Span::def_site());
         let f_64 = Ident::new("f64", Span::def_site());
         let string = Ident::new("String", Span::def_site());
+        let boolean = Ident::new("bool", Span::def_site());
+
         let i = ident.unwrap();
         let ident_name = {
             let i = i.as_ref();
@@ -166,10 +178,11 @@ fn impl_struct_xml_fields(name: &syn::Ident, fields: &syn::Fields) -> quote::Tok
                         TokenTree {
                             span: Span::def_site(),
                             kind: TokenNode::Literal(Literal::byte_string(ident_name.as_bytes())),
-                        }.into_tokens(),
+                        }
+                        .into_tokens(),
                     );
 
-                    result.push(quote!{
+                    result.push(quote! {
                         => {
                             #ident = u64::from_str(&val)?;
                         }
@@ -179,10 +192,11 @@ fn impl_struct_xml_fields(name: &syn::Ident, fields: &syn::Fields) -> quote::Tok
                         TokenTree {
                             span: Span::def_site(),
                             kind: TokenNode::Literal(Literal::byte_string(ident_name.as_bytes())),
-                        }.into_tokens(),
+                        }
+                        .into_tokens(),
                     );
 
-                    result.push(quote!{
+                    result.push(quote! {
                         => {
                             #ident = f64::from_str(&val)?;
                         }
@@ -192,17 +206,32 @@ fn impl_struct_xml_fields(name: &syn::Ident, fields: &syn::Fields) -> quote::Tok
                         TokenTree {
                             span: Span::def_site(),
                             kind: TokenNode::Literal(Literal::byte_string(ident_name.as_bytes())),
-                        }.into_tokens(),
+                        }
+                        .into_tokens(),
                     );
 
-                    result.push(quote!{
+                    result.push(quote! {
                         => {
                             #ident = val.to_string();
                         }
                     });
+                } else if i_type == boolean {
+                    result.push(
+                        TokenTree {
+                            span: Span::def_site(),
+                            kind: TokenNode::Literal(Literal::byte_string(ident_name.as_bytes())),
+                        }
+                        .into_tokens(),
+                    );
+
+                    result.push(quote! {
+                        => {
+                            #ident = bool::from_str(&val)?;
+                        }
+                    });
                 } else {
                     // Uncomment me to debug why some fields may be missing
-                    //println!("else: {:?} {:?}", ident, i_type);
+                    println!("else: {:?} {:?}", ident, i_type);
                 }
             }
             None => {
@@ -212,7 +241,7 @@ fn impl_struct_xml_fields(name: &syn::Ident, fields: &syn::Fields) -> quote::Tok
         }
     }
 
-    result.push(quote!{
+    result.push(quote! {
         _ => {
             debug!(
                 "unknown xml attribute: {}",
@@ -225,15 +254,17 @@ fn impl_struct_xml_fields(name: &syn::Ident, fields: &syn::Fields) -> quote::Tok
         TokenTree {
             span: Span::def_site(),
             kind: TokenNode::Op('}', Spacing::Joint),
-        }.into_tokens(),
+        }
+        .into_tokens(),
     );
     result.push(
         TokenTree {
             span: Span::def_site(),
             kind: TokenNode::Op('}', Spacing::Joint),
-        }.into_tokens(),
+        }
+        .into_tokens(),
     );
-    result.push(quote!{
+    result.push(quote! {
         Ok
     });
 
@@ -241,10 +272,11 @@ fn impl_struct_xml_fields(name: &syn::Ident, fields: &syn::Fields) -> quote::Tok
         TokenTree {
             span: Span::def_site(),
             kind: TokenNode::Op('(', Spacing::Joint),
-        }.into_tokens(),
+        }
+        .into_tokens(),
     );
 
-    result.push(quote!{
+    result.push(quote! {
         #name
     });
 
@@ -252,13 +284,14 @@ fn impl_struct_xml_fields(name: &syn::Ident, fields: &syn::Fields) -> quote::Tok
         TokenTree {
             span: Span::def_site(),
             kind: TokenNode::Op('{', Spacing::Joint),
-        }.into_tokens(),
+        }
+        .into_tokens(),
     );
 
     for field in fields.iter() {
         let ident = &field.ident;
 
-        result.push(quote!{
+        result.push(quote! {
             #ident: #ident,
         });
     }
@@ -267,13 +300,15 @@ fn impl_struct_xml_fields(name: &syn::Ident, fields: &syn::Fields) -> quote::Tok
         TokenTree {
             span: Span::def_site(),
             kind: TokenNode::Op('}', Spacing::Joint),
-        }.into_tokens(),
+        }
+        .into_tokens(),
     );
     result.push(
         TokenTree {
             span: Span::def_site(),
             kind: TokenNode::Op(')', Spacing::Joint),
-        }.into_tokens(),
+        }
+        .into_tokens(),
     );
 
     quote! {
